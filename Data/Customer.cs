e65ae411db4fc7f60956b4018796b1142b57d911
Travis <thoyle3293@gmail.com>
2017-08-31 13:04:08 -0700

    new

---
+++ Data/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project09.Data
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Order = new HashSet<Order>();
+        }
+
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public ICollection<Order> Order { get; set; }
+    }
+}
---
+++ Data/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Project09.Data
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            Order = new HashSet<Order>();
+        }
+
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        [JsonIgnore]
+        public ICollection<Order> Order { get; set; }
+    }
+}
